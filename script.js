de16517ee2fe2926b021989cbc6a0430c34d45a5
tayip bilal  tedik <53196914+BILDEK@users.noreply.github.com>
2024-06-23 23:20:26 +0300

    Update script.js
    adding dropdown menu js code
    
    Signed-off-by: tayip bilal  tedik <53196914+BILDEK@users.noreply.github.com>

--- script.js
+++ script.js
@@ -7,3 +7,15 @@
                 });
             });
         });
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.querySelector('.menu-icon');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuIcon.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+        menuIcon.classList.toggle('active');
+    });
+});
